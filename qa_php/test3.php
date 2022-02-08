126dbe5ba0bab560e824bda4d869ddbc9e8c8d5a
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 13:33:43 +0100

    Initial W

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,7 +1,7 @@
 <?php
     require('connection2.php');
 
-    foreach ($pdo->query('SELECT * FROM QA_Question') as $row) {
+    foreach ($pdo->query('SELECT * FROM QA_Question where id = 18') as $row) {
         echo json_encode($row);
         echo '<br>';
     }
