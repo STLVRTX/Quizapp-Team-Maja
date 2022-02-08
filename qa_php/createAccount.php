fdec33f9cd6ebdd0b90d5167a1c7271e3189c1d3
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-09 00:32:14 +0100

    Update createAccount.php

--- qa_php/createAccount.php
+++ qa_php/createAccount.php
@@ -8,7 +8,7 @@
         $password = $_POST['input_password'];
         $repeatPassword = $_POST['input_password_repeat'];
 
-        $result = $pdo->query("SELECT * FROM QA_Account;");
+        $result = $pdo->query("SELECT * FROM QA_Account WHERE Username='$name'");
         echo json_encode($result->fetchAll());
 
         #if(! $row){
