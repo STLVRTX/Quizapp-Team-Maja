ae6866bbe663fbd78bc2354bb271a835bcb12c63
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 17:14:46 +0100

    Update test3.php

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,9 +1,17 @@
 <?php
     require('connection2.php');
 
-    foreach (as $row) {
-        $row = $row->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($row);
-        echo '<br>';
-    }
+    #foreach (as $row) {
+    #    $row = $row->fetchAll(PDO::FETCH_ASSOC);
+    #    echo json_encode($row);
+    #    echo '<br>';
+    #}
+
+    $sth = $dbh->prepare("SELECT * FROM QA_Account");
+    $sth->execute();
+
+    /* Fetch all of the remaining rows in the result set */
+    print("Fetch all of the remaining rows in the result set:\n");
+    $result = $sth->fetchAll();
+    print_r($result);
 ?>
