759a8bad8e2714696aa68f5ba043eb47cc67c5f7
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 16:50:56 +0100

    Initial AB

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,7 +1,7 @@
 <?php
     require('connection2.php');
 
-    foreach ($pdo->query('SELECT * FROM QA_Account')->fetch_assoc() as $row) {
+    foreach ($pdo->query('SELECT * FROM QA_Account') as $row) {
         echo json_encode($row);
         echo '<br>';
     }
