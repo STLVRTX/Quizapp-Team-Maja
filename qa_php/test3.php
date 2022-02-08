25a8e303acbb13c8058aa1d91313d3ecbe0d33b1
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 16:56:06 +0100

    Initial AC

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -2,6 +2,7 @@
     require('connection2.php');
 
     foreach ($pdo->query('SELECT * FROM QA_Account') as $row) {
+        $result = $row->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($row);
         echo '<br>';
     }
