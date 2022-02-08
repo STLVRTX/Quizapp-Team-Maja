c5431ef99db021269a66dc42086ff71d7319ee19
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 17:05:54 +0100

    Initial AD

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,9 +1,9 @@
 <?php
     require('connection2.php');
 
-    foreach ($pdo->query('SELECT * FROM QA_Account') as $row) {
-        $result = $row->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($row);
+    $data = $pdo->query('SELECT * FROM QA_Account');
+
+    $result = $data->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($data);
         echo '<br>';
-    }
 ?>
