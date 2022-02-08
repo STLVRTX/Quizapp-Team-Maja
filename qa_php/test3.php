db102c7bf7a14559a0c4d36adffe58dab742f037
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 17:22:31 +0100

    Update test3.php

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,15 +1,9 @@
 <?php
     require('connection2.php');
 
-    #foreach (as $row) {
-    #    $row = $row->fetchAll(PDO::FETCH_ASSOC);
-    #    echo json_encode($row);
-    #    echo '<br>';
-    #}
+    $sql = 'SELECT * FROM QA_Account';
 
-    $sth = $pdo->prepare("SELECT * FROM QA_Account");
-    foreach ($sth->execute() as $row) {
-        $result = $row->fetchAll();
-        echo($result);
+    foreach ($pdo->query($sql, PDO::FETCH_ASSOC) as $row){
+        echo json_encode($row);
     }
 ?>
