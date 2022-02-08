493d76d0f8de2a1b78ab9031269eeb965a52329d
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 18:12:41 +0100

    Update test3.php

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -35,7 +35,7 @@
                         <td><input type="text" name="input_pw"></td>
                     </tr>
                     <tr>
-                        <td colspan="2"><input type="submit" name="submit"></td>
+                        <td colspan="2"><input type="submit" name="submit" style="display: block; margin: auto;"></td>
                     </tr>
                 </table>
             </form>
