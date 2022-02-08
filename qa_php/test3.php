967188e05620dc0858232c1153832ae64d9de4d0
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 13:24:31 +0100

    Initial Q

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,9 +1,9 @@
 <?php
     require('connection2.php');
 
-    foreach ($pdo->query('SELECT * FROM QA_Account where id=1') as $row) {
-        echo $row['Username'];
-    }
+    #foreach ($pdo->query('SELECT * FROM QA_Account where id=1') as $row) {
+    #    echo $row['Username'];
+    #}
 
     echo 'done';
 ?>
