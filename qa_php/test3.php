8a6b18d7d3b17a7d7397945b75f96d84dbf66f48
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 13:08:03 +0100

    Initial I

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -1,5 +1,25 @@
 <?php
+   $tns = "quizzapp_high"; 
 
-echo "Hello!";
+   $user = "QUIZZDEMO"; 
 
+   $password = "QuizzApp2506"; 
+   try { 
+
+        $conn = new PDO("oci:dbname=".$tns, $user, $password); 
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+        echo 'Connected to database'; 
+
+        foreach ($conn->query('SELECT * FROM QA_Account;') as $row) {
+            echo $row['Username'];
+        }
+
+   } 
+   catch(PDOException $e) { 
+
+        echo 'ERROR: ' . $e->getMessage(); 
+
+   } 
 ?>
