e7cce8bd53d5fe964f0688bc43aa99990ce4790f
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 17:29:00 +0100

    Update test3.php

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -3,18 +3,21 @@
 
     $sql = 'SELECT * FROM QA_Account';
 
-    foreach ($pdo->query($sql, PDO::FETCH_ASSOC) as $row){
-        echo json_encode($row);
-    }
+    #foreach ($pdo->query($sql, PDO::FETCH_ASSOC) as $row){
+    #    echo json_encode($row);
+    #}
 ?>
 
 <html>
 
-<head>
-</head>
-<body>
-<div style="position: absolute; top:50%; left:50%">
-Test
-</div>
-</body>
+    <head>
+
+    </head>
+    <body>
+        <div style="position: absolute; top:50%; left:50%">
+            <form method="post">
+                <input type="text" id="username">Username</input>
+            </form>
+        </div>
+    </body>
 </html>
