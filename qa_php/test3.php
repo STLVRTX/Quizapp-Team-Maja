ca8002a2ec4b97d4702199fc16823a906259a86c
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-08 13:27:58 +0100

    Initial T

--- qa_php/test3.php
+++ qa_php/test3.php
@@ -2,7 +2,8 @@
     require('connection2.php');
 
     foreach ($pdo->query('SELECT * FROM QA_Question') as $row) {
-        echo $row[1];
+        echo $row[3];
+        echo '<br>'
     }
 
     echo 'done';
