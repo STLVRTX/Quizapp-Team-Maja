67f0078648710ceefa535631fbb2ed1329a25499
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-09 13:46:32 +0100

    Update accounts.php

--- qa_php/api/accounts.php
+++ qa_php/api/accounts.php
@@ -3,6 +3,9 @@
     if($method == 'GET'){
         if($id){
             $data = DB::query("SELECT * FROM $tableName WHERE ACCOUNTID=:id", array(':id' => $id));
+            if($data != null){
+                echo json_encode($data);
+            }
         }
     }
 
