b9c38b7091d8e71b36510cc4fc154cc21499d2a3
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-09 13:41:26 +0100

    afasasf

--- qa_php/index.php
+++ qa_php/index.php
@@ -6,10 +6,26 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 $request_uri = $_SERVER['REQUEST_URI'];
 
+$tables = ['QA_Account'];
+
 $url = rtrim($request_uri, '/');
 $url = filter_var($url, FILTER_SANITIZE_URL);
 $url = explode('/', $url);
 
 $tableName = (string) $url[4];
 print_r($tableName);
+
+if($url[5] != null){
+    $id = (int) $url[5];
+}
+else {
+    $id = null;
+}
+
+if(in_array($tableName, $tables)){
+    include_once './api/account.php';
+}
+else {
+    echo 'table does not exist';
+}
 ?>
