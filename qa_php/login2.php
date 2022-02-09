b416aafc42558039ff6bf4d250247af4a27306ff
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-09 23:11:49 +0100

    Create login2.php

---
+++ qa_php/login2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSON-Aufgabe</title>
+        <script>
+            var xhr = null;
+
+            function getJSONFromDatabase(x){
+                xhr = new XMLHttpRequest();
+                xhr.open('GET', 'index.php/accounts/' + x, true);
+                xhr.onload = function(){
+                    document.getElementById('ausgabe').innerHTML = xhr.responseText;
+                }
+                xhr.send(null);
+            }
+
+            function fillForm() {
+                if(xhr.readyState == 4){
+                    var myObj = xhr.responseText;
+                    myObj = eval("(" + myObj + ")");
+                    document.getElementById('usr_name').setAttribute('value', myObj[0]['Username']);
+                    document.getElementById('usr_password').setAttribute('value', myObj[0]['Password']);
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <form method="post">
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td><Input type="text" id="usr_name" name="usr_name"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><Input type="text" id="usr_password" name="usr_password"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><Input type="submit" value="Save Data" name="submit" onclick="getJSONFromDatabase('usr_name')"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
