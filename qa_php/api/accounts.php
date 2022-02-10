e7b3db8d9bb65bbb97f22725a3d2116a1e7b0633
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-10 12:11:03 +0100

    Update accounts.php

--- qa_php/api/accounts.php
+++ qa_php/api/accounts.php
@@ -15,8 +15,8 @@
         if($_POST != null){
             extract($_POST);
             DB::query("INSERT INTO $tableName (USERNAME, PASSWORD, EMAIL) VALUES (:username, :password, :email)", array(':username' => $post_username, ':password' => $post_password, ':email' => $post_email));
-            $data = DB::query("SELECT * FROM $tableName ORDER BY ACCOUNTID DESC LIMIT 1");
-            echo 'success ' . $data;
+            #$data = DB::query("SELECT * FROM $tableName ORDER BY ACCOUNTID DESC LIMIT 1");
+            echo 'success '; #. $data;
         }
     }
 ?>
