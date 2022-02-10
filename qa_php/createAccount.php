7da243587a1061c34cea8659d88883b807418263
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-10 17:41:56 +0100

    Update createAccount.php

--- qa_php/createAccount.php
+++ qa_php/createAccount.php
@@ -43,7 +43,7 @@
 
                 xhr = new XMLHttpRequest();
                 xhr.open('POST', 'index.php/accounts/', true);
-                xhr.setRequestHeader('Content-type', 'application/x-www-form');
+                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 xhr.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     console.log(this.responseText);
