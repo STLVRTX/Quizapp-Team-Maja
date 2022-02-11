7cdd5295805576dff71c7597933313609a9b145d
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-11 11:51:38 +0100

    Update questions.php

--- qa_php/api/questions.php
+++ qa_php/api/questions.php
@@ -3,8 +3,8 @@
 
     #/index.php/questions/DIFFICULTYID/AMOUNT 
 
-    $difficultyID = (int) $url[6];
-    $amount = (int) $url[7];
+    $difficultyID = (int) $url[5];
+    $amount = (int) $url[6];
 
     echo 'DIFFICULTYID: ' . $difficultyID;
     echo '<br>AMOUNT: ' . $amount;
