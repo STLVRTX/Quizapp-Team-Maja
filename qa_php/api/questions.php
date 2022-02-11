4c510d0b9829c9e8c545bc73db16df1c8307d8b1
Seraph141 <janik.irlenbusch@edu.bib.de>
2022-02-11 12:28:43 +0100

    Update questions.php

--- qa_php/api/questions.php
+++ qa_php/api/questions.php
@@ -1,12 +1,10 @@
 <?php
     $tableName = 'QA_Question';
 
-    #/index.php/questions/DIFFICULTYID/AMOUNT 
-
-    $difficultyID = (int) $url[5];
+    $categoryID = (int) $url[5];
     $amount = (int) $url[6];
 
-    $data = DB::query("SELECT * FROM QA_Question WHERE DIFFICULTYID=:id", array(':id' => $difficultyID));
+    $data = DB::query("SELECT * FROM QA_Question WHERE CATEGORYID=:id", array(':id' => $categoryID));
     foreach($data as $row_question){
         echo '[';
         echo json_encode($row_question);
